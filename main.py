3735fdebf8bfaf8079473b51a823a1dc4871e9cd
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-05-04 18:29:05 +0300

    space garbage added

--- main.py
+++ main.py
@@ -81,6 +81,24 @@ async def animate_spaceship(canvas, row, column, frames, max_x, max_y, frame_row
         old_frame = frame
         await asyncio.sleep(0)
 
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
+    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    row = 0
+
+    while row < rows_number:
+        draw_frame(canvas, row, column, garbage_frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, garbage_frame, negative=True)
+        row += speed
+
+async def fill_orbit_with_garbage():
+    pass
+
 def draw(canvas):
     curses.curs_set(False)
     canvas.nodelay(True)
@@ -99,6 +117,26 @@ def draw(canvas):
 
     frames = [rocket_frame_1, rocket_frame_1, rocket_frame_2, rocket_frame_2]
 
+    with open('files/duck.txt', 'rt') as src:
+        duck = src.read()
+
+    with open('files/hubble.txt', 'rt') as src:
+        hubble = src.read()
+
+    with open('files/lamp.txt', 'rt') as src:
+        lamp = src.read()
+
+    with open('files/trash_large.txt', 'rt') as src:
+        trash_large = src.read()
+
+    with open('files/trash_small.txt', 'rt') as src:
+        trash_small = src.read()
+
+    with open('files/trash_xl.txt', 'rt') as src:
+        trash_xl = src.read()
+
+    garbage_frames = [duck, lamp, trash_large, trash_small, trash_xl ]
+
     coroutines = []
 
     for i in range(num_stars):
@@ -112,6 +150,12 @@ def draw(canvas):
     coroutines.append(coroutine)
 
     coroutine = fire(canvas, max_y - frame_rows, max_x / 2)
+    coroutines.append(coroutine)
+
+    coroutine = fly_garbage(canvas, max_x / 2, random.choice(garbage_frames))
+    coroutines.append(coroutine)
+
+    coroutine = fill_orbit_with_garbage()
     coroutines.append(coroutine)
 
     canvas.border()
